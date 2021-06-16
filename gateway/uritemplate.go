4483719b82e6d5507f9e10878985f648ccaa60ab
okb-okb <kosukeokabe1998@gmail.com>
2021-06-16 15:58:05 +0900

    fix: #16 パラメータ名とパラメータ値を対応付けられるようmapで保持

--- gateway/uritemplate.go
+++ gateway/uritemplate.go
@@ -18,24 +18,31 @@ func NewURITemplate(path string) *URITemplate {
 	u := new(URITemplate)
 	slice := strings.Split(path[1:], "/")
 	for _, v := range slice {
+		isparam := strings.HasPrefix(v, "{") && strings.HasSuffix(v, "}")
+		var value string
+		if isparam {
+			value = v[1 : len(v)-1]
+		} else {
+			value = v
+		}
 		u.path = append(u.path, block{
-			value:   v,
-			isparam: strings.HasPrefix(v, "{") && strings.HasSuffix(v, "}"),
+			value,
+			isparam,
 		})
 	}
 
 	return u
 }
 
-func (u *URITemplate) TemplateMatch(t URITemplate) (bool, []string) {
-	var params []string
+func (u *URITemplate) TemplateMatch(t URITemplate) (bool, map[string]string) {
+	params := make(map[string]string)
 	if len(u.path) != len(t.path) {
 		return false, nil
 	}
 
 	for i := 0; i < len(u.path); i++ {
 		if t.path[i].isparam {
-			params = append(params, u.path[i].value)
+			params[t.path[i].value] = u.path[i].value
 		} else if u.path[i].value != t.path[i].value {
 			return false, nil
 		}
@@ -53,21 +60,15 @@ func (u *URITemplate) JoinPath() string {
 	return strings.Join(s, "/")
 }
 
-func (u *URITemplate) Allocate(s []string) error {
-	var indices []int
-	for i, v := range u.path {
-		if v.isparam {
-			indices = append(indices, i)
+func (u *URITemplate) Allocate(m map[string]string) error {
+	for i, block := range u.path {
+		if block.isparam {
+			if v, ok := m[block.value]; !ok {
+				return errors.New("no such parameter")
+			} else {
+				u.path[i].value = v
+			}
 		}
-	}
-
-	if len(indices) != len(s) {
-		return errors.New("number of parameters doesn't match")
-	}
-
-	for i, v := range indices {
-		u.path[v].value = s[i]
-		u.path[v].isparam = false
 	}
 
 	return nil
