2b8995ddde7ec9a587624d0b2027acad824f59aa
okb-okb <kosukeokabe1998@gmail.com>
2021-07-09 13:16:31 +0900

    feat: #24 タイムアウトやヘッダのサイズの制限を環境変数から設定可能に変更

--- gateway/cmd/main/main.go
+++ gateway/cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"local.packages/gateway"
 
@@ -16,5 +17,22 @@ func main() {
 		r.Delete("/*", gateway.DeleteHandler)
 		r.Post("/*", gateway.PostHandler)
 	})
-	http.ListenAndServe(":3000", r)
+
+	rt := GetEnvWithDeault("READTIMEOUT", 5)
+	rht := GetEnvWithDeault("READHEADERTIMEOUT", 5)
+	wt := GetEnvWithDeault("WRITETIMEOUT", 5)
+	it := GetEnvWithDeault("IDLETIMEOUT", 5)
+	mhb := GetEnvWithDeault("MAXHEADERBYTES", 1<<20)
+
+	s := &http.Server{
+		Addr:              ":3000",
+		Handler:           r,
+		ReadTimeout:       time.Duration(rt) * time.Second,
+		ReadHeaderTimeout: time.Duration(rht) * time.Second,
+		WriteTimeout:      time.Duration(wt) * time.Second,
+		IdleTimeout:       time.Duration(it) * time.Second,
+		MaxHeaderBytes:    mhb,
+	}
+
+	s.ListenAndServe()
 }
