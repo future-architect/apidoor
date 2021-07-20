c04f6bc694af69867de17b44256cfff6a8e9963b
okb-okb <kosukeokabe1998@gmail.com>
2021-07-20 18:08:30 +0900

    test: #19 テストのデバッグ、ケースの追加

--- management-front/tests/unit/get-products.spec.ts
+++ management-front/tests/unit/get-products.spec.ts
@@ -1,11 +1,13 @@
-import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import equal from "fast-deep-equal";
 
 import { getProducts } from "@/api/get-products";
-import productsData from "./__mocks__/products.json";
+import validProductsData from "./__mocks__/get-products-valid.json";
+import invalidProductsData from "./__mocks__/get-products-invalid.json";
+import instance from "@/api/instance";
 
 describe("Test API Handler", () => {
-  const mock = new MockAdapter(axios);
+  const mock = new MockAdapter(instance, { onNoMatch: "throwException" });
 
   afterEach(() => {
     mock.reset();
@@ -13,11 +15,14 @@ describe("Test API Handler", () => {
 
   describe("test get-products", () => {
     it("should succeed", async () => {
-      mock.onGet("/products").reply(200, productsData);
-
+      mock.onGet("/products").reply(200, validProductsData);
       const data = await getProducts();
-
-      expect(data.products[0].id).toBe(3);
+      expect(equal(data, validProductsData)).toBeTruthy();
+    });
+    it("should return empty list of products", async () => {
+      mock.onGet("/products").reply(200, invalidProductsData);
+      const data = await getProducts();
+      expect(equal(data, { products: [] })).toBeTruthy();
     });
   });
 });
