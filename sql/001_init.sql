3e4746ad52b5b4e5c7326fbd4647a0ea215c143d
okb-okb <kosukeokabe1998@gmail.com>
2021-09-16 22:36:14 +0900

    feat: add SQL to initialize DB for log

--- sql/001_init.sql
+++ sql/001_init.sql
@@ -13,4 +13,15 @@ WITH (
 
 COMMENT ON TABLE public.apiinfo
     IS 'Store information of products(API).';
+
+CREATE TABLE IF NOT EXISTS public.log_list
+(
+    run_date timestamp with time zone NOT NULL,
+    api_key text NOT NULL,
+    api_path text NOT NULL,
+    custom_log jsonb
+);
+
+COMMENT ON TABLE public.log_list
+    IS 'Table to save log of gateway.';
 END;
